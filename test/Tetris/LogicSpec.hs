fc73bc805d51cc0b188978baa31766db59fe816f
Attila Domokos <adomokos@gmail.com>
2018-03-07 22:31:34 -0600

    Draw shapes by providing used cells

--- test/Tetris/LogicSpec.hs
+++ test/Tetris/LogicSpec.hs
@@ -1,7 +1,56 @@
 module Tetris.LogicSpec where
 
+import Prelude hiding (Left, Right)
 import Test.Hspec
-import qualified Tetris.Logic as L
+import Data.Set
+{- import qualified Tetris.Logic as L -}
+
+{-
+   The board is represented as a 10x10 matrix.
+   The columns and rows are numbered from 0-9,
+   the bottom left cell is represented as (0,0),
+   the top right cell is represented as (9,9).
+-}
+
+data Position = Start | Left | Right | Up | Down deriving (Show, Eq)
+type Shape = [Position]
+
+type Row = Int
+type Column = Int
+type Cell = (Row, Column)
+type Board = [Cell]
+
+q :: Shape
+q = [Start,Right,Up,Left]
+
+z :: Shape
+z = [Start,Right,Down,Right]
+
+s :: Shape
+s = [Start,Right,Up,Right]
+
+t :: Shape
+t = [Start,Right,Down,Up,Right]
+
+i :: Shape
+i = [Start,Right,Right,Right]
+
+l :: Shape
+l =[Start,Right,Left,Up,Up]
+
+j :: Shape
+j =[Start,Right,Up,Up]
+
+placeOnBoard :: Shape -> Column -> Board -> Board
+placeOnBoard shape column board = []
+
+drawShape :: Cell -> Shape -> Board -> Board
+drawShape _ [] [] = []
+drawShape cell (Start:xs) board = cell : drawShape cell xs board
+drawShape (r,c) (Right:xs) board = (r,c+1) : drawShape (r,c+1) xs board
+drawShape (r,c) (Up:xs) board = (r+1,c) : drawShape (r+1,c) xs board
+drawShape (r,c) (Down:xs) board = (r-1,c) : drawShape (r-1,c) xs board
+drawShape (r,c) (Left:xs) board = (r,c-1) : drawShape (r,c-1) xs board
 
 main :: IO ()
 main = hspec spec
@@ -10,4 +59,11 @@ spec :: Spec
 spec =
     describe "Tetris Logic" $ do
         it "can position elements" $ do
-            L.someFunc `shouldBe` "someFunc"
+            drawShape (0,0) q [] `shouldBe` [(0,0), (0,1), (1,1), (1,0)]
+            drawShape (0,0) i [] `shouldBe` [(0,0), (0,1), (0,2), (0,3)]
+            drawShape (0,3) i [] `shouldBe` [(0,3), (0,4), (0,5), (0,6)]
+            drawShape (1,0) t [] `shouldBe` [(1,0), (1,1), (0,1), (1,1), (1,2)]
+            drawShape (1,0) z [] `shouldBe` [(1,0), (1,1), (0,1), (0,2)]
+            drawShape (0,0) s [] `shouldBe` [(0,0), (0,1), (1,1), (1,2)]
+            drawShape (0,1) l [] `shouldBe` [(0,1), (0,2), (0,1), (1,1), (2,1)]
+            drawShape (0,2) j [] `shouldBe` [(0,2), (0,3), (1,3), (2,3)]
